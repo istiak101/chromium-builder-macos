531e4bda9a9e10bec6f251eeb31c385075750c41
istiak101 <30789544+istiak101@users.noreply.github.com>
2023-09-11 09:54:56 +0600

    Initial commit

---
+++ .github/scripts/github_before_build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -eux
+
+# Simple script for downloading, unpacking, and getting ready to build Ungoogled-Chromium macOS binaries on GitHub Actions
+
+_root_dir=$(dirname $(greadlink -f $0))
+_download_cache="$_root_dir/build/download_cache"
+_src_dir="$_root_dir/build/src"
+_main_repo="$_root_dir/chromium-build-system"
+
+mkdir -p "$_src_dir"
+sudo df -h
+sudo du -hs "$_src_dir"
+
+rm -rf "$_src_dir/out" || true
+mkdir -p "$_src_dir/out/Default"
+mkdir -p "$_download_cache"
+
+"$_main_repo/utils/downloads.py" retrieve -i "$_main_repo/downloads.ini" "$_root_dir/downloads.ini" -c "$_download_cache"
+"$_main_repo/utils/downloads.py" unpack -i "$_main_repo/downloads.ini" "$_root_dir/downloads.ini" -c "$_download_cache" "$_src_dir"
+"$_main_repo/utils/prune_binaries.py" "$_src_dir" "$_main_repo/pruning.list"
+"$_main_repo/utils/patches.py" apply "$_src_dir" "$_main_repo/patches" "$_root_dir/patches"
+"$_main_repo/utils/domain_substitution.py" apply -r "$_main_repo/domain_regex.list" -f "$_main_repo/domain_substitution.list" "$_src_dir"
+
+shopt -s nocasematch
+if [[ $GITHUB_REF =~ arm || $(git log --pretty='%s' -1) =~ arm  ]]; then
+  echo 'target_cpu = "arm64"' >> "$_root_dir/flags.macos.gn"
+  # sudo xcode-select -s "/Applications/Xcode_13.4.app"
+fi
+
+cp "$_main_repo/flags.gn" "$_src_dir/out/Default/args.gn"
+cat "$_root_dir/flags.macos.gn" >> "$_src_dir/out/Default/args.gn"
+
+cd "$_src_dir"
+
+./tools/gn/bootstrap/bootstrap.py -o out/Default/gn --skip-generate-buildfiles
+./out/Default/gn gen out/Default --fail-on-unused-args
+
+rm -rvf "$_download_cache"
